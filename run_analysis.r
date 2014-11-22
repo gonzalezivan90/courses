21e28da28b37237812c64be04a0ad6a222e50c2e
gonzalezivan90 <igonzalez@humboldt.org.co>
2014-11-22 16:11:12 -0500

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -47,7 +47,11 @@ sel.features.names <- gsub(",", "_", gsub('\\(|\\)', '', gsub("\\(\\)", "", sel.
 
 dataset <- rbind(data.frame(train, as.matrix(train_X_train[, sel.features])), data.frame(test, as.matrix(test_X_test[, sel.features])))
 dataset$recordID <- 1:nrow(dataset)
-colnames(dataset) <- c(colnames(train), sel.features.names2)
+colnames(dataset) <- c(colnames(train), sel.features.names)
+
+head(dataset)
+str(dataset)
+summary(dataset)
 
 ## 7. Make the second tidy data set 
 
@@ -61,5 +65,8 @@ names2 <- matrix(unlist(strsplit(rownames(dataset2), '-')), nrow = length(unique
 colnames(names2) <- c('subID', 'actName')
 
 tidydataset <- cbind(names2, dataset2)
+head(tidydataset)
+str(tidydataset)
+summary(tidydataset)
 
 write.table(tidydataset, "tidydataset.txt", row.names = FALSE)
